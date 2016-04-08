5c07cc47cd706051f88cf39e6782fbd273b7a5bb
Dave Nash <dave@cordump.com>
2016-04-08 15:23:28 -0400

    working simple insertion sort

--- python/sorting_comparison/sort.py
+++ python/sorting_comparison/sort.py
@@ -3,3 +3,6 @@ import sorters
 
 bubble = sorters.bubbleSort(["banana","orange","apple","Grape","zucchini","mango"])
 print bubble
+
+insert = sorters.insertionSort(["banana","orange","apple","Grape","zucchini","mango"])
+print insert
--- python/sorting_comparison/sorters.py
+++ python/sorting_comparison/sorters.py
@@ -25,3 +25,22 @@ def bubbleSort(sort_list):
         sortComplete = True
 # return the sorted list
   return(sort_list)
+
+def insertionSort(sort_list):
+  returnList = [] # Initialize the return list
+  for item in sort_list: # start the loop of the orginal list
+    if not returnList: # If the list is empty add the current item to start the list
+      returnList.append(item)
+    else:
+      for index,new_item in enumerate(returnList): # run through a loop to determine where to add the item from sort_list to returnList
+        # if the item from sort_list is lower than the itme form returnList and we are not at the end of the list insert item at the current index
+        if item <= new_item  and index < len(returnList): 
+          returnList.insert(index, item)
+          break
+        # if item from sort_list is greater than the item from returnList and we are at the end of returnList append the item 
+        elif item >= new_item and index + 1 == len(returnList):
+          returnList.append(item)
+          break
+  #return the sorted list to the caller
+  return(returnList )
+    
